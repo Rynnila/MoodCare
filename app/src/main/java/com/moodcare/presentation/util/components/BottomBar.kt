133c2152cfc13b123e93eda8ca6d4c1d66ad57e6
GuilhermeBanik <guilherme.banik.dev@gmail.com>
2025-03-27 15:58:23 -0300

    Mudança no BottomBar.kt

--- app/src/main/java/com/moodcare/presentation/util/components/BottomBar.kt
+++ app/src/main/java/com/moodcare/presentation/util/components/BottomBar.kt
@@ -3,25 +3,23 @@ package com.moodcare.presentation.util.components
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.pager.PagerState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.moodcare.ui.theme.onPrimary
 import com.moodcare.ui.theme.primary
 
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun BottomBar(items: List<NavItem>, onItemSelected: (NavItem) -> Unit) {
     BottomAppBar(
-        modifier = Modifier.height(60.dp),
+        modifier = Modifier
+            .height(60.dp),
         containerColor = primary()
     ) {
         items.forEach { navItem ->
@@ -34,12 +32,16 @@ fun BottomBar(items: List<NavItem>, onItemSelected: (NavItem) -> Unit) {
                 },
                 icon = {
                     Icon(
+                        tint = onPrimary(),
                         painter = painterResource(id = navItem.icon),
                         contentDescription = navItem.label
                     )
                 },
                 label = {
-                    Text(navItem.label)
+                    Text(
+                        text = navItem.label,
+                        color = onPrimary()
+                    )
                 }
             )
         }
